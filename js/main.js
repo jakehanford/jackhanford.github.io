5844d12e3269a07d0f30fade0a403a0a0983984c
Jack Hanford <jackhanford@gmail.com>
2013-12-09 15:27:48 -0800

    Maybe?

--- js/main.js
+++ js/main.js
@@ -6,14 +6,38 @@ $(document).ready(function() {
     $(this).fadeOut("fast")
     $(".toppadd").slideUp();
   });
+
   $(document).ready(function() {
-    $('.slideshow img:gt(0)').fadeOut();
+
+    $('.slideshow img:gt(index)').fadeOut();
       setInterval(function(){
         $('.slideshow :first-child').fadeIn()
           .next('img').fadeOut()
           .end().appendTo('.slideshow');},
-        3000);
-    });
+        2000);
+
+    $('.slideshow2 img:gt(index)').fadeOut();
+      setInterval(function(){
+        $('.slideshow2 :first-child').fadeIn()
+          .next('img').fadeOut()
+          .end().appendTo('.slideshow2');},
+        2500)
+
+    $('.slideshow3 img:gt(index)').fadeOut();
+      setInterval(function(){
+        $('.slideshow3 :first-child').fadeIn()
+          .next('img').fadeOut()
+          .end().appendTo('.slideshow3');},
+        3500)
+
+    $('.slideshow4 img:gt(index)').fadeOut();
+      setInterval(function(){
+        $('.slideshow4 :first-child').fadeIn()
+          .next('img').fadeOut()
+          .end().appendTo('.slideshow4');},
+        3000)
+  });
+
   $('.resumetoggle').click(function() {
     $('.resume').toggle();
   });
